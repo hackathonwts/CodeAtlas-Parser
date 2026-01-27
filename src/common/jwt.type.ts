7955e902d0d7728db52c4bbb3c7871b61778caf0
Rabbul Ansary <itsrabbul@gmail.com>
2026-01-27 16:11:41 +0530

    remove unused constants, exception filter, decorators, and JWT type definitions

--- src/common/jwt.type.ts
+++
@@ -1,4 +0,0 @@
-export type JwtPayloadType = {
-    id: string;
-    iat?: string;
-};
--- src/common/role.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { CustomDecorator, SetMetadata } from '@nestjs/common';
-import { UserRoleEnum } from 'src/role/schemas/role.schema';
-
-export const Roles = (...roles: UserRoleEnum[]): CustomDecorator<string> => {
-    return SetMetadata('roles', roles);
-};
