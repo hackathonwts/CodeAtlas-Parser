da19ce663c232aee55eee025bdab1b06d4a1a65e
Rabbul Ansary <itsrabbul@gmail.com>
2026-02-05 15:31:10 +0530

    feat: implement topic existence check and create missing Kafka topics on module initialization

--- src/kafka/kafka.service.ts
+++ src/kafka/kafka.service.ts
@@ -2,15 +2,31 @@ import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import type { KafkaClient } from './kafka.type';
 import { KAFKA_CLIENT } from './kafka.constants';
 import { KAFKA_TOPICS } from './kafka.topics';
+import { ITopicConfig } from 'kafkajs';
 
 @Injectable()
 export class KafkaService implements OnModuleInit {
-    constructor(@Inject(KAFKA_CLIENT) private readonly kafka: KafkaClient) {}
+    constructor(@Inject(KAFKA_CLIENT) private readonly kafka: KafkaClient) { }
 
     async onModuleInit() {
-        await this.kafka.admin.createTopics({
-            waitForLeaders: true,
-            topics: Object.values(KAFKA_TOPICS),
-        });
+        try {
+            const existingTopics = await this.kafka.admin.listTopics();
+            const missingTopics: ITopicConfig[] = [];
+
+            for (const topicConfig of Object.values(KAFKA_TOPICS)) {
+                const topicName = topicConfig.topic;
+                if (!existingTopics.includes(topicName)) {
+                    missingTopics.push(topicConfig);
+                }
+            }
+            if (missingTopics.length > 0) {
+                await this.kafka.admin.createTopics({
+                    waitForLeaders: true,
+                    topics: missingTopics,
+                });
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
 }
